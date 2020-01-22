5323998d2980a4f0bd43f717e03b19a71a037dcd
David Van Fleet <dvfleet413@gmail.com>
2020-01-22 21:39:45 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
   
   if array.size > 1
     last_element = array.pop
-    return "#{array.join(', ')" + " and #{last_element}"
+    return "#{array.join + " and #{last_element}"
   else
     return array.join(', ')
   end
